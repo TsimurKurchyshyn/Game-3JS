217f7630f2b07da50a9947fadc651374f375ba55
Tsimur <dopingjr2010@gmail.com>
2023-12-03 15:30:05 -0500

    First part of program.

--- Game3.js
+++ Game3.js
@@ -6,6 +6,7 @@ widthInBlocks - Ширина в пикселях.
 heightInBlocks - Высота в пикселях.
 score - Счёт сколько яблок съедено.
 drawBorder - Нарисовать рамку canvas.
+gameOver - Надпись которая появится после окончания игры.
 */
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
@@ -25,12 +26,26 @@ let drawBorder = function () {
      ctx.fillRect(0, 0, blockSize, height);
      ctx.fillRect(width - blockSize, 0, blockSize, height);
 };
-var drawScore = function () {
+let drawScore = function () {
     ctx.font = "20px Counter";
 ctx.fillStyle = "Black";
 ctx.textAlign = "Left";
 ctx.textBaseline = "top";
-ctx.fillText("Счёт: " + score, blockSize, blockSize);
+ctx.fillText("Счёт: " + score, 50, blockSize);
 };
+let gameOver = function () {
+    ctx.font = "60px Courier";
+    ctx.fillStyle = "Black";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText("Конец игры", width / 2, height / 2);
+};
+setInterval(function (){
+ctx.clearRect(0, 0, width, height);
 drawBorder();
 drawScore();
+score++;
+}, 100);
+drawBorder();
+drawScore();
+gameOver();
